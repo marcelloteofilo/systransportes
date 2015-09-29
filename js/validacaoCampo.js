59cfda8e73b8f558873ddfa9f845fc1fcac02df5
Thiagoadsunibratec <thiago.ads.unibratec01@gmail.com>
2015-09-29 20:12:54 -0300

    despesas alteração de layout

--- js/validacaoCampo.js
+++ js/validacaoCampo.js
@@ -173,3 +173,17 @@ function MascaraCNPJ(cnpj){
 
 }
 
+function mascaraData(campoData){
+              var data = campoData.value;
+              if (data.length == 2){
+                  data = data + '/';
+                  document.forms[0].data.value = data;
+      return true;              
+              }
+              if (data.length == 5){
+                  data = data + '/';
+                  document.forms[0].data.value = data;
+                  return true;
+              }
+         }
+
