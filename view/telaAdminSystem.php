b7f9dd1c9625739237b736d9765634b8c71feffa
Thiagoadsunibratec <thiago.ads.unibratec01@gmail.com>
2015-10-24 13:31:05 -0300

    Comentei parte delogin
    Comentei a parte que josenaldo fez para ter acesso direto ao pagina do
    adm.

--- view/telaAdminSystem.php
+++ view/telaAdminSystem.php
@@ -1,13 +1,13 @@
 <!--<?php
-    session_start();
-if (!isset($_SESSION['login']) == true and !isset($_SESSION['senha']) == true) {
-    session_destroy();
-    unset($_SESSION['login']);
-    unset($_SESSION['senha']);
-    header('location: usuario/login.php#login');
-} else {
-    $logado = $_SESSION['login'];
-}
+   //session_start();
+//if (!isset($_SESSION['login']) == true and !isset($_SESSION['senha']) == true) {
+   // session_destroy();
+   // unset($_SESSION['login']);
+    //unset($_SESSION['senha']);
+   // header('location: usuario/login.php#login');
+//} else {
+  //  $logado = $_SESSION['login'];
+//}
 ?>-->
 
 <!DOCTYPE html>
