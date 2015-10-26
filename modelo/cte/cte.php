2cdefb913d0236b87dc8191a2c7fa51e4a4d4817
Luis Alberto <luis.lasn@gmail.com>
2015-10-26 11:12:48 -0300

    Criação da classe cte.php
    Criação da classe cte.php com seus respectivos atributos e métodos
    getters e setters.

--- modelo/cte/cte.php
+++ modelo/cte/cte.php
@@ -1,111 +1,69 @@
 <?php
   class Cte {
 
-  //Atributos
-  private $id;    
-  private $idUsuario;
-  private $idVeiculo;
-  private $cotacao;
-  private $manifesto;
-  private $faturamento;
-  private $emissao;
-  private $status;
-  private $dataEntrega;
-  private $horaEntrega;
-   
-      
-  public function setId($l) {
-    $this->id = trim($l);
-  }
+  	//Atributos
+  	private $numeroCte;
+  	private $codigoCarga;
+    private $codigoRota;
+  	private $situacao;
+  	private $chaveAcesso;
+  	private $statusCte;
+  	private $emissao;
+
+  	public function setNumeroCte($numeroCte) {
+    	$this->numeroCte = trim($numeroCte);
+  	}
     
-  public function getId() {
-    return $this->id;
-  }
-   public function setUsuario($usuario) {
-       $this->usuario = trim($usuario);
-  }
-  
-  public function getUsuario() {     
-    if($this->usuario == null){
-      $this->usuario = new Usuario();
-    }   
-    return $this->usuario;
-  } 
-  public function setVeiculo($veiculo) {
-       $this->veiculo = trim($veiculo);
-  }
-  
-  public function getVeiculo() {     
-    if($this->veiculo == null){
-      $this->veiculo = new Veiculo();
-    }   
-    return $this->veiculo;
-  } 
+  	public function getNumeroCte() {
+    	return $this->numeroCte;
+  	}
 
-  public function setCotacao($cotacao) {
-       $this->cotacao = trim($cotacao);
-  }
-  
-  public function getCotacao() {     
-    if($this->cotacao == null){
-      $this->cotacao = new Cotacao();
-    }   
-    return $this->cotacao;
-  } 
+  	public function setCodigoCarga($codigoCarga) {
+  		$this->codigoCarga = trim($codigoCarga);
+  	}
 
-  public function setManifesto($manifesto) {
-       $this->manifesto = trim($manifesto);
-  }
-  
-  public function getManifesto() {     
-    if($this->manifesto == null){
-      $this->manifesto = new Manifesto();
-    }   
-    return $this->manifesto;
-  } 
+  	public function getCodigoCarga() {
+  		return $this->codigoCarga;
+  	}
 
-  public function setFaturamento($faturamento) {
-       $this->faturamento = trim($faturamento);
-  }
-  
-  public function getFaturamento() {     
-    if($this->faturamento == null){
-      $this->faturamento = new Manifesto();
-    }   
-    return $this->faturamento;
-  } 
-  public function setEmissao($emissao) {
-    $this->id = trim($emissao);
-  }
-    
-  public function getEmissao() {
-    return $this->emissao;
-  }
+    public function setCodigoRota($codigoRota) {
+      $this->codigoRota = trim($codigoRota);
+    }
 
-  public function setStatus($status) {
-    $this->id = trim($status);
-  }
-    
-  public function getStatus() {
-    return $this->status;
-  }
+    public function getCodigoRota() {
+      return $this->codigoRota;
+    }
 
-	public function setDataEntrega($dataEntrega) {
-    $this->id = trim($dataEntrega);
-  }
-    
-  public function getDataEntrega() {
-    return $this->dataEntrega;
-  }
+  	public function setSituacao($situacao) {
+  		$this->situacao = trim($situacao);
+  	}
 
-  public function setHoraEntrega($horaEntrega) {
-    $this->id = trim($horaEntrega);
-  }
-    
-  public function getHoraEntrega() {
-    return $this->horaEntrega;
-  }	
+  	public function getSituacao() {
+  		return $this->situacao;
+  	}
 
-  }
+  	public function setChaveAcesso($chaveAcesso) {
+  		$this->chaveAcesso = trim($chaveAcesso);
+  	}
 
-?>
+  	public function getChaveAcesso() {
+  		return $this->chaveAcesso;
+  	}
+
+  	public function setStatusCte($statusCte) {
+  		$this->statusCte = trim($statusCte);
+  	}
+
+  	public function getStatusCte() {
+  		return $this->statusCte;
+  	}
+
+  	public function setEmissao($emissao) {
+  		$this->emissao = trim($emissao);
+  	}
+
+  	public function getEmissao() {
+  		return $this->sEmissao;
+  	}
+
+ }
