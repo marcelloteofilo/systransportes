e7a8c467aa6db02a3bb86b19244202b8b69e5a4b
marcelloteofilo <marcello.312@hotmail.com>
2015-10-26 19:42:57 -0300

    Coleta Basica

--- modelo/coleta/coleta.php
+++ modelo/coleta/coleta.php
@@ -1,29 +1,18 @@
 <?php
 	require_once("/../usuario/usuario.php");  
-	require_once("/../cotacao/cotacao.php");  
+	require_once("/../carga/carga.php");  
 	require_once("/../veiculo/veiculo.php");  
 	
   class Coleta {
-<<<<<<< HEAD
-    private $codColeta;
-	private $emissao;  	
-	private $objRemetente;  	
-	private $objDestinatario;  	
-	private $objMotorista;  	
-	private $objCotacao;  	
-	private $objVeiculo;  	
-	private $dataAgendada;  	
-	private $horaAgendada;  	
-	private $obs;  	
-	private $status;  	
-=======
-    private $id;  
-    private $codCarga;
+
+  private $codColeta;  
+  private $codCarga;
 	private $codMotorista;  	
 	private $codVeiculo;  
 	private $data;   
-	private $hora;   
-	private $statusCarga;  	
+	private $hora;
+	   
+	private $coletada;  	
 	private $telefone;  	 
 	private $logradouro;   
 	private $bairro; 
@@ -31,14 +20,13 @@
 	private $estado;  	
 	private $cidade;
 	private $observacao;  	  	
->>>>>>> origin/master
             
-    public function setId($l) {
-      $this->id = trim($l);
+    public function setCodColeta($codColeta) {
+      $this->codColeta = trim($codColeta);
     }
     
-    public function getId() {
-      return $this->id;
+    public function getCodColeta() {
+      return $this->codColeta;
     }	
 
 
@@ -48,7 +36,7 @@
 	
 	public function CodCarga() {     //da uma olhada depois
 		if($this->codCarga == null){
-			$this->codCarga = new Cotacao();
+			$this->codCarga = new Carga();
 		}		
 		return $this->codCarga;
 	}
@@ -91,12 +79,13 @@
       return $this->hora;
     }	
 
-    public function setStatusCarga($statusCarga) {
-      $this->statusCarga = trim($statusCarga);
+//////////////////////////// CARGA /////////////////////////////////////////////////
+    public function setColetada($coletada) {
+      $this->coletada = trim($coletada);
     }
     
-    public function getStatusCarga() {
-      return $this->statusCarga;
+    public function getColetada() {
+      return $this->coletada;
     }	
 
     public function setTelefone($telefone) {
@@ -154,97 +143,5 @@
     public function getObservacao() {
       return $this->observacao;
     }
-
-	/*
-	public function setObjRemetente($objRemetente) {
-		   $this->objRemetente = trim($objRemetente);
-	}
-	
-	public function getObjRemetente() {     
-		if($this->objRemetente == null){
-			$this->objRemetente = new Usuario();
-		}		
-		return $this->objRemetente;
-	}	
-	
-	public function setObjDestinatario($objDestinatario) {
-		   $this->objDestinatario = trim($objDestinatario);
-	}
-	
-	public function getObjDestinatario() {     
-		if($this->objDestinatario == null){
-			$this->objDestinatario = new Usuario();
-		}		
-		return $this->objDestinatario;
-	}	
-	
-	
-	public function setObjMotorista($objMotorista) {
-		   $this->objMotorista = trim($objMotorista);
-	}
-	
-	public function getObjMotorista() {     
-		if($this->objMotorista == null){
-			$this->objMotorista = new Usuario();
-		}		
-		return $this->objMotorista;
-	}	
-	
-	
-	public function setObjCotacao($objCotacao) {
-		   $this->objCotacao = trim($objCotacao);
-	}
-	
-	public function getObjCotacao() {     
-		if($this->objCotacao == null){
-			$this->objCotacao = new Cotacao();
-		}		
-		return $this->objCotacao;
-	}	
-	
-	
-	public function setObjVeiculo($objVeiculo) {
-		   $this->objVeiculo = trim($objVeiculo);
-	}
-	
-	public function getObjVeiculo() {     
-		if($this->objVeiculo == null){
-			$this->objVeiculo = new Veiculo();
-		}		
-		return $this->objVeiculo;
-	}	
-	
-	public function setDataAgendada($l) {
-      $this->dataAgendada = trim($l);
-    }
-    
-    public function getDataAgendada() {
-      return $this->dataAgendada;
-    }	
-	
-	public function setHoraAgendada($l) {
-      $this->horaAgendada = trim($l);
-    }
-    
-    public function getHoraAgendada() {
-      return $this->horaAgendada;
-    }	
-		
-	public function setObs($l) {
-      $this->obs = trim($l);
-    }
-    
-    public function getObs() {
-      return $this->obs;
-    }	
-	
-	public function setStatus($l) {
-      $this->status = trim($l);
-    }
-    
-    public function getStatus() {
-      return $this->status;
-    }	
-	*/  
   }
 ?>
